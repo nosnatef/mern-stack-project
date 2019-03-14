91db7f0512824df780313686993fd1790f16e64f
nosnatef <gushua1984@gmail.com>
2019-03-14 02:16:33 -0700

    Version 0.0.1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const songs = require('./routes/api/songs');
 
@@ -18,6 +19,16 @@ mongoose.connect(db,{ useNewUrlParser: true })
 
 //Use Routes
 app.use('/api/songs',songs);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV == 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
